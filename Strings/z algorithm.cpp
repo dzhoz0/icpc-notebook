ca4a38204b05de901caeee88d236d668131c9953
Hughie <156927506+Born-To-Laugh@users.noreply.github.com>
2026-08-03 17:38:51 +0700

    Update z algorithm.cpp

--- Strings/z algorithm.cpp
+++ Strings/z algorithm.cpp
@@ -1,24 +1,15 @@
-vector<int> compute_z(const string &s){
-  int n = s.size();
-  vector<int> z(n,0);
-  int l,r;
-  r = l = 0;
-  for(int i = 1; i < n; ++i){
-    if(i > r) {
-      l = r = i;
-      while(r < n and s[r - l] == s[r])r++;
-      z[i] = r - l;r--;
-    }else{
-      int k = i-l;
-      if(z[k] < r - i +1) z[i] = z[k];
-      else {
-        l = i;
-        while(r < n and s[r - l] == s[r])r++;
-        z[i] = r - l;r--;
-      }
+vector<int> z_function(string s) {
+    int n = s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r) z[i] = min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
     }
-  }
-  return z;
+    return z;
 }
 /*
  * alfalfa
