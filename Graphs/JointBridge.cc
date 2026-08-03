70f01006580b2ce9bd42c32824173875e0d5f9d3
Hughie <156927506+Born-To-Laugh@users.noreply.github.com>
2026-08-03 19:35:36 +0700

    Update JointBridge.cc

--- Graphs/JointBridge.cc
+++ Graphs/JointBridge.cc
@@ -1,10 +1,3 @@
-// Born_To_Laugh - Hughie Do
-#include <bits/stdc++.h>
-#define alle(Lotus) Lotus.begin(), Lotus.end()
-using namespace std;
-typedef long long ll;
-[[maybe_unused]] const ll MOD = 998244353, INF = 1e9 + 7;
-#define int ll
 const int maxn = 1e4 + 10;
 int n, m;
 vector<int> adj[maxn];
@@ -33,21 +26,4 @@ void dfs(int a, int par){
     ok |= (par == -1 && child > 1);
     if(ok) cntj++;
     tout[a] = id;
-}
-void solve(){
-    cin >> n >> m;
-    for(int i=1; i<=m; ++i){
-        int a, b;cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    for(int i=1; i<=n; ++i){
-        if(!tin[i])dfs(i, -1);
-    }
-    cout << cntj << ' ' << cntb;
-}
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    solve();
 }
