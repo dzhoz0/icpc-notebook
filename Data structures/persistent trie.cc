313e75996e245966b6632f623ee9bf119623fe52
Hughie <156927506+Born-To-Laugh@users.noreply.github.com>
2026-08-03 17:24:57 +0700

    Update persistent trie.cc

--- Data structures/persistent trie.cc
+++ Data structures/persistent trie.cc
@@ -1,90 +1,59 @@
-// both tries can be tested with the problem: http://codeforces.com/problemset/problem/916/D
-
-
-
-// Persistent binary trie (BST for integers)
-const int MD = 31;
-
-struct node_bin {
-  node_bin *child[2];
-  int val;
-
-  node_bin() : val(0) {
-    child[0] = child[1] = NULL;
-  }
-};
-
-typedef node_bin* pnode_bin;
-
-pnode_bin copy_node(pnode_bin cur) {
-  pnode_bin ans = new node_bin();
-  if (cur) *ans = *cur;
-  return ans;
-}
-
-pnode_bin modify(pnode_bin cur, int key, int inc, int id = MD) {
-  pnode_bin ans = copy_node(cur);
-  ans->val += inc;
-  if (id >= 0) {
-    int to = (key >> id) & 1;
-    ans->child[to] = modify(ans->child[to], key, inc, id - 1);
-  }
-  return ans;
-}
-
-int sum_smaller(pnode_bin cur, int key, int id = MD) {
-  if (cur == NULL) return 0;
-  if (id < 0) return 0;  // strictly smaller
-  // if (id == - 1) return cur->val;  // smaller or equal
-
-  int ans = 0;
-  int to = (key >> id) & 1;
-  if (to) {
-    if (cur->child[0]) ans += cur->child[0]->val;
-    ans += sum_smaller(cur->child[1], key, id - 1);
-  } else {
-    ans = sum_smaller(cur->child[0], key, id - 1);
-  }
-  return ans;
-}
-
-
-// Persistent trie for strings.
-const int MAX_CHILD = 26;
-struct node {
-  node *child[MAX_CHILD];
-  int val;
-  node() : val(-1) {
-    for (int i = 0; i < MAX_CHILD; i++) {
-      child[i] = NULL;
+struct PerTrie{
+    struct node{
+        int cnt = 0;
+        array<int, 2> child;
+        node(){
+            child.fill(-1);
+        }
+    };
+    int id = 0;
+    vector<node> tree;
+    PerTrie(){
+        tree.emplace_back();
     }
-  }
+    int new_node(){
+        ++id;
+        if(id >= tree.size())tree.emplace_back();
+        return id;
+    }
+    int add(int a, int prev){
+        int cc = new_node();
+        int cur = cc;
+        tree[cur] = tree[prev];
+        tree[cur].cnt++;
+        for(int i=30; i>=0; --i){
+            int val = (a >> i) & 1;
+            int nxt = new_node();
+
+            if(prev != -1 && tree[prev].child[val] != -1){
+                tree[nxt] = tree[tree[prev].child[val]];
+                prev = tree[prev].child[val];
+            }
+            else prev = -1;
+
+            tree[cur].child[val] = nxt;
+            cur = nxt;
+            tree[cur].cnt++;
+        }
+        return cc;
+    }
+    int walk(int prev, int cur, int a){
+        int ans = 0;
+        for(int i=30; i>=0; --i){
+            int val = 1 - ((a >> i) & 1);
+            int num1 = (cur != -1 && tree[cur].child[val] != -1) ? tree[tree[cur].child[val]].cnt : 0;
+            int num2 = (prev != -1 && tree[prev].child[val] != -1) ? tree[tree[prev].child[val]].cnt : 0;
+            if(num1 - num2 > 0){
+                ans |= (val << i);
+                cur = (cur != -1) ? tree[cur].child[val] : -1;
+                prev = (prev != -1) ? tree[prev].child[val] : -1;
+            }
+            else{
+                ans |= ((1 - val) << i);
+                cur = (cur != -1) ? tree[cur].child[1 - val] : -1;
+                prev = (prev != -1) ? tree[prev].child[1 - val] : -1;
+            }
+        }
+        return ans;
+    }
 };
-
-typedef node* pnode;
-
-pnode copy_node(pnode cur) {
-  pnode ans = new node();
-  if (cur) *ans = *cur;
-  return ans;
-}
-
-pnode set_val(pnode cur, string &key, int val, int id = 0) {
-  pnode ans = copy_node(cur);
-  if (id >= int(key.size())) {
-    ans->val = val;
-  } else {
-    int t = key[id] - 'a';
-    ans->child[t] = set_val(ans->child[t], key, val, id + 1);
-  }
-  return ans;
-}
-
-pnode get(pnode cur, string &key, int id = 0) {
-  if (id >= int(key.size()) || !cur)
-    return cur;
-  int t = key[id] - 'a';
-  return get(cur->child[t], key, id + 1);
-}
-
-
