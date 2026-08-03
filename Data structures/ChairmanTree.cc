92e824bc7155fbb8d772375ca58ca9ceed27010f
Hughie <156927506+Born-To-Laugh@users.noreply.github.com>
2026-08-03 19:24:56 +0700

    Refactor PersistentSegTree to Chairman_Tree
    Refactor PersistentSegTree into Chairman_Tree with updated methods for point updates and queries.

--- Data structures/ChairmanTree.cc
+++ Data structures/ChairmanTree.cc
@@ -1,66 +1,61 @@
-struct PersistentSegTree {
-    struct Node {
-        int val = 0;
-        int l = -1, r = -1;
-
-        Node() {}
-        Node(int v) : val(v) {}
-        Node(int v, int L, int R) : val(v), l(L), r(R) {}
-
-        Node operator+(const Node &a, const Node &b) {
-            return Node(a.val + b.val, a.l, b.r);
+class Chairman_Tree
+{
+private:
+    struct node{
+        int l, r;
+        int val;
+        node(){
+            l = 0;
+            r = 0;
+            val = 0;
         }
     };
-
-    vector<Node> st;
-    int n, id;
-
-    void init(int _n){
-        n = _n; 
-        st.resize(20 * _n + 5);
-    }
-
-    int add_copy(int root) {
-        st.push_back(st[root]);
-        return id++;
-    }
-
-    int build(int l, int r) {
-        int my = id++;
-        st.emplace_back();
-        if (l == r) return my;
-
+    int n;
+    vector<node> tree;
+    int cntnode = 0;
+    int build(int l, int r){
+        int id = ++cntnode;
+        if(l == r)return id;
         int mid = (l + r) >> 1;
-        int left = build(l, mid);
-        int right = build(mid + 1, r);
-        st[my] = st[left] + st[right];
-        st[my].l = left;
-        st[my].r = right;
-        return my;
+        tree[id].l = build(l, mid);
+        tree[id].r = build(mid+1, r);
+        return id;
     }
-
-    int update(int cur, int l, int r, int k, int x) {
-        if (k < l || k > r) return cur;
-        if (l == r) {
-            st.emplace_back(x);
-            return id++;
+    int Point_Update(int cur, int l, int r, int pos){
+        int id = ++cntnode;
+        tree[id] = tree[cur];
+        if(l == r){
+            tree[id].val++;
+            return id;
         }
-
         int mid = (l + r) >> 1;
-        int newLeft = update(st[cur].l, l, mid, k, x);
-        int newRight = updateRec(st[cur].r, mid + 1, r, k, x);
-        st.emplace_back(st[newLeft] + st[newRight]);
-        st.back().l = newLeft;
-        st.back().r = newRight;
-        return id++;
+        if(pos <= mid){
+            tree[id].l = Point_Update(tree[cur].l, l, mid, pos);
+        }
+        else{
+            tree[id].r = Point_Update(tree[cur].r, mid + 1, r, pos);
+        }
+        tree[id].val = tree[tree[id].l].val + tree[tree[id].r].val;
+        return id;
     }
-
-    int queryRec(int cur, int l, int r, int L, int R) {
-        if (R < l || r < L) return 0;
-        if (L <= l && r <= R) return st[cur].val;
+    int get(int id, int l, int r, int lo, int hi){
+        if(l > hi || r < lo)return 0;
+        else if(l >= lo && r <= hi){
+            return tree[id].val;
+        }
         int mid = (l + r) >> 1;
-        return queryRec(st[cur].l, l, mid, L, R) +
-               queryRec(st[cur].r, mid + 1, r, L, R);
+        return get(tree[id].l, l, mid, lo, hi) + get(tree[id].r, mid + 1, r, lo, hi);
+    }
+public:
+    int init(int len){
+        n = len;
+        tree.assign(20 * n, node());
+        return build(1, n);
+    }
+    int Point_Update(int root, int pos){
+        return Point_Update(root, 1, n, pos);
+    }
+    int get(int root, int l, int r){
+        return get(root, 1, n, l, r);
     }
 };
-
