c5906e3ea489ad5b367180cae32ef98d755cfc77
Hughie <156927506+Born-To-Laugh@users.noreply.github.com>
2026-08-03 17:22:23 +0700

    Refactor heavy light decomposition with segment tree

--- Data structures/heavy light decomposition.cc
+++ Data structures/heavy light decomposition.cc
@@ -1,69 +1,122 @@
-// Heavy-Light Decomposition
-struct TreeDecomposition {
-  vector<int> g[MAXN], c[MAXN];
-  int s[MAXN]; // subtree size
-  int p[MAXN]; // parent id
-  int r[MAXN]; // chain root id
-  int t[MAXN]; // index used in segtree/bit/...
-  int d[MAXN]; // depht
-  int ts;
-
-  void dfs(int v, int f) {
-    p[v] = f;
-    s[v] = 1;
-    if (f != -1) d[v] = d[f] + 1;
-    else d[v] = 0;
-
-    for (int i = 0; i < g[v].size(); ++i) {
-      int w = g[v][i];
-      if (w != f) {
-        dfs(w, v);
-        s[v] += s[w];
-      }
-    }
-  }
-
-  void hld(int v, int f, int k) {
-    t[v] = ts++;
-    c[k].push_back(v);
-    r[v] = k;
-
-    int x = 0, y = -1;
-    for (int i = 0; i < g[v].size(); ++i) {
-      int w = g[v][i];
-      if (w != f) {
-        if (s[w] > x) {
-          x = s[w];
-          y = w;
+// Born_To_Laugh - Hughie Do
+#include <bits/stdc++.h>
+#define alle(AC) AC.begin(), AC.end()
+using namespace std;
+typedef long long ll;
+[[maybe_unused]] const ll MOD = 998244353, INF = 1e9 + 7;
+const int maxn = 5e4 + 10;
+class segtree
+{
+private:
+    int n;
+    vector<int> tree;
+    vector<int> lazy;
+    void operate(int id, int l, int r){
+        if(!lazy[id])return;
+        tree[id] += lazy[id];
+        if(l != r){
+            lazy[id << 1] += lazy[id];
+            lazy[id << 1|1] += lazy[id];
         }
-      }
+        lazy[id] = 0;
     }
-    if (y != -1) {
-      hld(y, v, k);
+    void upd(int id, int l, int r, int lo, int hi, int val){
+        operate(id, l, r);
+        if(l > hi || r < lo)return;
+        else if(l >= lo && r <= hi){
+            lazy[id] += val;
+            operate(id, l, r);
+            return;
+        }
+        int mid = (l + r) >> 1;
+        upd(id << 1, l, mid, lo, hi, val);
+        upd(id << 1|1, mid+1, r, lo, hi, val);
+        tree[id] = max(tree[id << 1], tree[id << 1|1]);
     }
-
-    for (int i = 0; i < g[v].size(); ++i) {
-      int w = g[v][i];
-      if (w != f && w != y) {
-        hld(w, v, w);
-      }
+public:
+    segtree(){
+        n = maxn;
+        tree.assign(n << 2, 0);
+        lazy.assign(n << 2, 0);
     }
-  }
-
-  void init(int n) {
-    for (int i = 0; i < n; ++i) {
-      g[i].clear();
+    void upd(int lo, int hi){
+        upd(1, 1, n, lo, hi, 1);
     }
-  }
-
-  void add(int a, int b) {
-    g[a].push_back(b);
-    g[b].push_back(a);
-  }
-
-  void build() {
-    ts = 0;
-    dfs(0, -1);
-    hld(0, 0, 0);
-  }
+    int get(){
+        return tree[1];
+    }
 };
+segtree st;
+int n, q;
+vector<int> adj[maxn];
+int tin[maxn], chain[maxn], head[maxn], parr[maxn], big[maxn], dep[maxn], sz[maxn];
+int id = 0;
+int cur = 1;
+void predfs(int a, int par){
+    sz[a] = 1;
+    for(auto &elm: adj[a]){
+        if(elm == par)continue;
+        dep[elm] = dep[a] + 1;
+        predfs(elm, a);
+        sz[a] += sz[elm];
+        if(sz[elm] > sz[big[a]])big[a] = elm;
+    }
+}
+void hld(int a, int par, int root){
+    tin[a] = ++id;
+    chain[a] = cur;
+    parr[a] = par;
+    head[a] = root;
+    if(big[a])hld(big[a], a, root);
+    for(auto &elm: adj[a]){
+        if(elm == par || elm == big[a])continue;
+        cur++;
+        hld(elm, a, elm);
+    }
+}
+int lca(int a, int b){
+    while(chain[a] != chain[b]){
+        if(chain[a] < chain[b])swap(a, b);
+        a = parr[head[a]];
+    }
+    if(dep[a] > dep[b])swap(a, b);
+    return a;
+}
+void ope(int a, int b){
+    int c = lca(a, b);
+    // cout << a << ' ' << b << ' ' << c << '\n';
+    // return;
+    // int c = 0;
+    while(chain[a] != chain[c]){
+        st.upd(tin[head[a]], tin[a]);
+        a = parr[head[a]];
+    }
+    while(chain[b] != chain[c]){
+        st.upd(tin[head[b]], tin[b]);
+        b = parr[head[b]];
+    }
+    if(dep[a] > dep[b])swap(a, b);
+    st.upd(tin[a], tin[b]);
+}
+void solve(){
+    cin >> n >> q;
+    for(int i=1; i<n; ++i){
+        int a, b;cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    predfs(1, -1);
+    hld(1, -1, 1);
+    for(int i=1; i<=q; ++i){
+        int a, b;cin >> a >> b;
+        ope(a, b);
+    }
+    cout << st.get();
+}
+signed main(){
+    freopen("maxflow.in", "r", stdin);
+    freopen("maxflow.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
