046b1417dc61a7a286e8d92405634a9360d59180
dzhoz0 <datuanho2k9@gmail.com>
2026-08-15 20:25:47 +0700

    hello

--- gen.sh
+++ gen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-notebook-generator . --columns 3 --size 8 -a "HSG-DiagonalBottle" -I ./DiagonalBottle.png
+notebook-generator . --columns 3 --size 8 -a "HSG-DiagonalBottle" -I ./DiagonalBottle.png -i "HSG-DiagonalBottle"
